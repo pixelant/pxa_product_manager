74d1bdbbc4ca32128f93f58f3aee5b4333a6ed32
Mattias Nilsson <mattias@pixelant.se>
2018-11-30 08:13:03 +0100

    [TASK] Update requirements to TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '9.3.18',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7-9.4.99'
+            'typo3' => '8.7.0-9.5.99'
         ],
         'conflicts' => [],
         'suggests' => []
