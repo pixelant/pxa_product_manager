f9c59731d16fbfd75811a7b34bc84a0f414337c4
anjeylink <andriy.oprysko@resultify.se>
2018-12-28 10:19:52 +0200

    [TASK] tests for json attributes

--- Tests/Functional/Domain/Repository/ProductRepositoryTest.php
+++ Tests/Functional/Domain/Repository/ProductRepositoryTest.php
@@ -25,6 +25,7 @@ class ProductRepositoryTest extends FunctionalTestCase
     {
         parent::setUp();
         $this->importDataSet(__DIR__ . '/../../Fixtures/tx_pxaproductmanager_domain_model_product.xml');
+        $this->importDataSet(__DIR__ . '/../../Fixtures/tx_pxaproductmanager_domain_model_option.xml');
         $this->productRepository = GeneralUtility::makeInstance(ObjectManager::class)->get(ProductRepository::class);
     }
 
@@ -89,5 +90,126 @@ class ProductRepositoryTest extends FunctionalTestCase
             Query::class,
             $query
         );
+    }
+
+    /**
+     * @test
+     */
+    public function getProductByMatchAttributeValueUsingQueryParserReturnProduct()
+    {
+        $query = $this->productRepository->createQuery();
+
+        $query->getQuerySettings()->setRespectStoragePage(false);
+
+        $query->matching(
+            $query->equals(
+                'attributesValues->1',
+                '200'
+            )
+        );
+
+        $sql = $this->productRepository->convertQueryBuilderToSql($query);
+
+        $product = $query->statement($sql)->execute();
+
+        $this->assertEquals(100, $product->getFirst()->getUid());
+    }
+
+    /**
+     * @test
+     */
+    public function getProductByContainAttributeValueUsingQueryParserReturnProduct()
+    {
+        $query = $this->productRepository->createQuery();
+
+        $query->getQuerySettings()->setRespectStoragePage(false);
+
+        $query->matching(
+            $query->contains(
+                'attributesValues->3',
+                '2'
+            )
+        );
+
+        $sql = $this->productRepository->convertQueryBuilderToSql($query);
+
+        $product = $query->statement($sql)->execute();
+
+        $this->assertEquals(1, $product->getFirst()->getUid());
+    }
+
+    /**
+     * @test
+     */
+    public function getProductByLikeAttributeValueUsingQueryParserReturnProduct()
+    {
+        $query = $this->productRepository->createQuery();
+
+        $query->getQuerySettings()->setRespectStoragePage(false);
+
+        $query->matching(
+            $query->like(
+                'attributesValues->1',
+                'test value'
+            )
+        );
+
+        $sql = $this->productRepository->convertQueryBuilderToSql($query);
+
+        $product = $query->statement($sql)->execute();
+
+        $this->assertEquals(1, $product->getFirst()->getUid());
+    }
+
+    /**
+     * @test
+     */
+    public function getProductByInAttributeValueUsingQueryParserReturnProduct()
+    {
+        $query = $this->productRepository->createQuery();
+
+        $query->getQuerySettings()->setRespectStoragePage(false);
+
+        $query->matching(
+            $query->in(
+                'attributesValues->2',
+                [
+                    2,
+                    1,
+                    3
+                ]
+            )
+        );
+
+        $sql = $this->productRepository->convertQueryBuilderToSql($query);
+
+        $product = $query->statement($sql)->execute();
+
+        $this->assertEquals(100, $product->getFirst()->getUid());
+    }
+
+    /**
+     * @test
+     */
+    public function getProductByRangeAttributeValueUsingQueryParserReturnProduct()
+    {
+        $query = $this->productRepository->createQuery();
+
+        $query->getQuerySettings()->setRespectStoragePage(false);
+
+        // Find where options value between given range
+        $query->matching(
+            $query->attributesRange(
+                'attributesValues->3',
+                190,
+                200
+            )
+        );
+
+        $sql = $this->productRepository->convertQueryBuilderToSql($query);
+
+        $product = $query->statement($sql)->execute();
+
+        $this->assertEquals(1, $product->getFirst()->getUid());
     }
 }
