cfb3c3da62aee930a267cc48da15d6bc8980ff6c
anjeylink <andriy.oprysko@resultify.se>
2019-04-22 16:01:16 +0300

    [TASK] use link builder service in typo link builder

--- Classes/LinkHandler/ProductLinkBuilder.php
+++ Classes/LinkHandler/ProductLinkBuilder.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+
 namespace Pixelant\PxaProductManager\LinkHandler;
 
 /***************************************************************
@@ -25,60 +26,15 @@ namespace Pixelant\PxaProductManager\LinkHandler;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-use Pixelant\PxaProductManager\Domain\Repository\CategoryRepository;
-use Pixelant\PxaProductManager\Domain\Repository\ProductRepository;
+use Pixelant\PxaProductManager\Service\Link\LinkBuilderService;
 use Pixelant\PxaProductManager\Utility\ConfigurationUtility;
-use Pixelant\PxaProductManager\Utility\MainUtility;
 use TYPO3\CMS\Core\Http\ImmediateResponseException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Configuration\AbstractConfigurationManager;
-use TYPO3\CMS\Extbase\Configuration\BackendConfigurationManager;
-use TYPO3\CMS\Extbase\Configuration\FrontendConfigurationManager;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
-use TYPO3\CMS\Extbase\Service\EnvironmentService;
-use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-use TYPO3\CMS\Frontend\Typolink\AbstractTypolinkBuilder;
 use TYPO3\CMS\Frontend\Controller\ErrorController;
+use TYPO3\CMS\Frontend\Typolink\AbstractTypolinkBuilder;
 
 class ProductLinkBuilder extends AbstractTypolinkBuilder
 {
-    /**
-     * @var ProductRepository
-     */
-    protected $productRepository;
-
-    /**
-     * @var CategoryRepository
-     */
-    protected $categoryRepository;
-
-    /**
-     * @var AbstractConfigurationManager
-     */
-    protected $configurationManager;
-
-    /**
-     * AbstractTypolinkBuilder constructor.
-     *
-     * @param $contentObjectRenderer ContentObjectRenderer
-     */
-    public function __construct(ContentObjectRenderer $contentObjectRenderer)
-    {
-        parent::__construct($contentObjectRenderer);
-        /** @var ObjectManager $objectManager */
-        $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
-
-        $this->productRepository = $objectManager->get(ProductRepository::class);
-        $this->categoryRepository = $objectManager->get(CategoryRepository::class);
-
-        $environmentService = $objectManager->get(EnvironmentService::class);
-        if ($environmentService->isEnvironmentInFrontendMode()) {
-            $this->configurationManager = $objectManager->get(FrontendConfigurationManager::class);
-        } else {
-            $this->configurationManager = $objectManager->get(BackendConfigurationManager::class);
-        }
-    }
-
     /**
      * Generates link to product single view
      *
@@ -92,19 +48,7 @@ class ProductLinkBuilder extends AbstractTypolinkBuilder
     {
         $finalUrl = '';
 
-        $this->checkExtbaseMapping();
-
-        if (isset($linkDetails['product'])
-            && $product = $this->productRepository->findByUid((int)$linkDetails['product'])
-        ) {
-            $parameters = MainUtility::buildLinksArguments($product);
-        } elseif (isset($linkDetails['category'])
-            && $category = $this->categoryRepository->findByUid((int)$linkDetails['category'])
-        ) {
-            $parameters = MainUtility::buildLinksArguments(null, $category);
-        }
-
-        if (isset($parameters)) {
+        if (isset($linkDetails['product']) || isset($linkDetails['category'])) {
             $singleViewPageUid = ConfigurationUtility::getSettingsByPath('pagePid');
 
             if (empty($singleViewPageUid) && !empty($GLOBALS['TYPO3_REQUEST'])) {
@@ -121,52 +65,13 @@ class ProductLinkBuilder extends AbstractTypolinkBuilder
                 }
             }
 
-            $confLink = [
-                'parameter' => $singleViewPageUid ?: MainUtility::getTSFE()->id,
-                'useCacheHash' => 1,
-                'additionalParams' => '&' . http_build_query($parameters),
-            ];
+            $linkBuilder = GeneralUtility::makeInstance(LinkBuilderService::class);
 
-            $finalUrl = $this->contentObjectRenderer->typolink_URL($confLink);
+            $finalUrl = isset($linkDetails['product'])
+                ? $linkBuilder->buildForProduct((int)$singleViewPageUid, (int)$linkDetails['product'])
+                : $linkBuilder->buildForCategory((int)$singleViewPageUid, (int)$linkDetails['category']);
         }
 
         return [$finalUrl, $linkText, $target];
-    }
-
-    /**
-     * Checks if extbase mapping is set for PM Category to sys_category
-     *
-     * Workaround to prevent the wrong tablename getting written
-     * in cf_extbase_datamapfactory_datamap for identifier
-     * 'Pixelant%PxaProductManager%Domain%Model%Category'
-     * when LinkBuilder is called from middleware in TYPO3 v9,
-     * e.g. the first request after cache is cleared is a redirec to a Category (PM).
-     *
-     * Since extbase ts configuration isn't loaded when ProductLinkBuilder
-     * is called from middleware, mapping for
-     * 'Pixelant\PxaProductManager\Domain\Model\Category' to 'sys_category'
-     * isn't set and TYPO3 will then assume that the table name
-     * for object is 'tx_pxaproductmanager_domain_model_category'.
-     *
-     * Once set in cache TYPO3 will try to load categories from
-     * 'tx_pxaproductmanager_domain_model_category' instead from 'sys_category'
-     * and exception will be thrown.
-     *
-     * This will make sure the mapping for
-     * 'Pixelant\PxaProductManager\Domain\Model\Category' to 'sys_category'
-     * exists when buildDataMapInternal in DataMapFactory is executed.
-     *
-     * @return void
-     */
-    protected function checkExtbaseMapping()
-    {
-        if (!MainUtility::isBelowTypo3v9()) {
-            $className = 'Pixelant\PxaProductManager\Domain\Model\Category';
-            $configuration = $this->configurationManager->getConfiguration(null, null);
-            if (empty($configuration['persistence']['classes'][$className]['mapping']['tableName'])) {
-                $pmCategoryMapping['persistence']['classes'][$className]['mapping']['tableName'] = 'sys_category';
-                $this->configurationManager->setConfiguration($pmCategoryMapping);
-            }
-        }
     }
 }
