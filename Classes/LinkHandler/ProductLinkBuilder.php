100e5e53aa4aacafbbb8a0f123e304a6dc6cf573
anjeylink <andriy.oprysko@resultify.se>
2019-05-08 12:03:22 +0300

    [TASK] rebuilder product link builder in order to get work correct in BE redirect module

--- Classes/LinkHandler/ProductLinkBuilder.php
+++ Classes/LinkHandler/ProductLinkBuilder.php
@@ -28,14 +28,39 @@ namespace Pixelant\PxaProductManager\LinkHandler;
 
 use Pixelant\PxaProductManager\Service\Link\LinkBuilderService;
 use Pixelant\PxaProductManager\Utility\ConfigurationUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Exception\SiteNotFoundException;
 use TYPO3\CMS\Core\Http\ImmediateResponseException;
+use TYPO3\CMS\Core\Site\Entity\NullSite;
 use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Site\Entity\SiteInterface;
+use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Controller\ErrorController;
 use TYPO3\CMS\Frontend\Typolink\AbstractTypolinkBuilder;
 
 class ProductLinkBuilder extends AbstractTypolinkBuilder
 {
+    /**
+     * Link builder modes
+     */
+    const PRODUCT_MODE = 1;
+    const CATEGORY_MODE = 2;
+
+    /**
+     * Product or category UID
+     *
+     * @var int
+     */
+    protected $recordUid = 0;
+
+    /**
+     * Determinate builder mode
+     *
+     * @var int
+     */
+    protected $mode = 0;
+
     /**
      * Generates link to product single view
      *
@@ -49,29 +74,26 @@ class ProductLinkBuilder extends AbstractTypolinkBuilder
     {
         $finalUrl = '';
 
-        if (isset($linkDetails['product']) || isset($linkDetails['category'])) {
-            $singleViewPageUid = ConfigurationUtility::getSettingsByPath('pagePid');
+        $this->defineBuilderMode($linkDetails);
 
-            if (empty($singleViewPageUid) && !empty($GLOBALS['TYPO3_REQUEST'])) {
-                /** @var Site $site */
-                $site = $GLOBALS['TYPO3_REQUEST']->getAttribute('site');
-                $singleViewPageUid = $site->getConfiguration()['productSingleViewFallbackPid'];
+        $singleViewPageUid = ConfigurationUtility::getSettingsByPath('pagePid');
 
-                if (empty($singleViewPageUid) || (int)$singleViewPageUid === 0) {
-                    $response = GeneralUtility::makeInstance(ErrorController::class)->pageNotFoundAction(
-                        $GLOBALS['TYPO3_REQUEST'],
-                        'The requested product single view page was not found',
-                        ['The fallback pid is not set']
-                    );
-                    throw new ImmediateResponseException($response, 1533931329);
-                }
-            }
+        if (empty($singleViewPageUid) && ($site = $this->getSite())) {
+            $singleViewPageUid = $site->getConfiguration()['productSingleViewFallbackPid'];
+        }
 
+        $singleViewPageUid = (int)$singleViewPageUid;
+        if ($singleViewPageUid > 0) {
             $linkBuilder = $this->getLinkBuilder();
 
-            $finalUrl = isset($linkDetails['product'])
-                ? $linkBuilder->buildForProduct((int)$singleViewPageUid, (int)$linkDetails['product'])
-                : $linkBuilder->buildForCategory((int)$singleViewPageUid, (int)$linkDetails['category']);
+            switch ($this->mode) {
+                case self::PRODUCT_MODE:
+                    $finalUrl = $linkBuilder->buildForProduct($singleViewPageUid, $this->recordUid);
+                    break;
+                case self::CATEGORY_MODE:
+                    $finalUrl = $linkBuilder->buildForCategory($singleViewPageUid, $this->recordUid);
+                    break;
+            }
         }
 
         return [$finalUrl, $linkText, $target];
@@ -89,5 +111,70 @@ class ProductLinkBuilder extends AbstractTypolinkBuilder
             null, // Detect language automatically
             $this->getTypoScriptFrontendController() // Pass give TypoScriptFrontendController
         );
+    }
+
+    /**
+     * Define link builder mode
+     *
+     * @param array $linkDetails
+     */
+    protected function defineBuilderMode(array $linkDetails): void
+    {
+        if (isset($linkDetails['product'])) {
+            $this->mode = self::PRODUCT_MODE;
+            $this->recordUid = (int)$linkDetails['product'];
+
+            return;
+        }
+
+        if (isset($linkDetails['category'])) {
+            $this->mode = self::CATEGORY_MODE;
+            $this->recordUid = (int)$linkDetails['category'];
+
+            return;
+        }
+
+        // @codingStandardsIgnoreStart
+        throw new \InvalidArgumentException('Product or Category shall be provided for product link builder', 1557304991448);
+        // @codingStandardsIgnoreEnd
+    }
+
+    /**
+     * Get site entry from request or try to find by record Pid
+     *
+     * @return Site|null
+     */
+    protected function getSite(): ?Site
+    {
+        if (isset($GLOBALS['TYPO3_REQUEST'])) {
+            /** @var Site $site */
+            $site = $GLOBALS['TYPO3_REQUEST']->getAttribute('site');
+        }
+        // Try to find site by record PID
+        if (isset($site) && $site instanceof NullSite) {
+            $table = $this->mode === self::PRODUCT_MODE
+                ? 'tx_pxaproductmanager_domain_model_product'
+                : 'sys_category';
+
+            $pid = GeneralUtility::makeInstance(ConnectionPool::class)
+                ->getConnectionForTable($table)
+                ->select(
+                    ['pid'],
+                    $table,
+                    ['uid' => $this->recordUid]
+                )
+                ->fetchColumn(0);
+            
+            if ($pid > 0) {
+                $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+                try {
+                    return $siteFinder->getSiteByPageId((int)$pid);
+                } catch (SiteNotFoundException $exception) {
+                    // Just return null
+                }
+            }
+        }
+
+        return null;
     }
 }
