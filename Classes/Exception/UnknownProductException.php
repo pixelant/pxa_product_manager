386c181fb3a3f4593245cc22149e99a57ea4ceed
mabolek <mathias@lilio.com>
2019-11-15 11:45:43 +0100

    [TASK] Created UnknownProductException

---
+++ Classes/Exception/UnknownProductException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pixelant\PxaProductManager\Exception;
+
+/**
+ * Class UnknownProductException
+ * @package Pixelant\PxaProductManager\Exception
+ */
+class UnknownProductException extends \Exception
+{
+
+}
