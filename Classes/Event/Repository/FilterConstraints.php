1b9367daf3c123c88f556e79afee51652ebe4df3
Andriy Oprysko <pyvasyk@gmail.com>
2020-03-30 16:40:54 +0300

    [TASK] new events classes

---
+++ Classes/Event/Repository/FilterConstraints.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace Pixelant\PxaProductManager\Event\Repository;
+
+use Pixelant\PxaProductManager\Domain\Model\DTO\DemandInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
+/**
+ * @package Pixelant\PxaProductManager\Event\Repository
+ */
+class FilterConstraints
+{
+    /**
+     * @var DemandInterface
+     */
+    protected DemandInterface $demand;
+
+    /**
+     * @var QueryInterface
+     */
+    protected QueryInterface $query;
+    /**
+     * @var array
+     */
+    protected array $constraints;
+
+    /**
+     * @param DemandInterface $demand
+     * @param QueryInterface $query
+     * @param array $constraints
+     */
+    public function __construct(DemandInterface $demand, QueryInterface $query, array $constraints)
+    {
+        $this->demand = $demand;
+        $this->query = $query;
+        $this->constraints = $constraints;
+    }
+
+    /**
+     * @return DemandInterface
+     */
+    public function getDemand(): DemandInterface
+    {
+        return $this->demand;
+    }
+
+    /**
+     * @return QueryInterface
+     */
+    public function getQuery(): QueryInterface
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConstraints(): array
+    {
+        return $this->constraints;
+    }
+
+    /**
+     * @param array $constraints
+     * @return FilterConstraints
+     */
+    public function setConstraints(array $constraints): FilterConstraints
+    {
+        $this->constraints = $constraints;
+        return $this;
+    }
+}
---
+++ Classes/Event/Repository/RepositoryDemand.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Pixelant\PxaProductManager\Event\Repository;
+
+use Pixelant\PxaProductManager\Domain\Model\DTO\DemandInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
+/**
+ * @package Pixelant\PxaProductManager\Event\Product
+ */
+class RepositoryDemand
+{
+    /**
+     * @var DemandInterface
+     */
+    protected DemandInterface $demand;
+
+    /**
+     * @var QueryInterface
+     */
+    protected QueryInterface $query;
+
+    /**
+     * @param DemandInterface $demand
+     * @param QueryInterface $query
+     */
+    public function __construct(DemandInterface $demand, QueryInterface $query)
+    {
+        $this->demand = $demand;
+        $this->query = $query;
+    }
+
+    /**
+     * @return DemandInterface
+     */
+    public function getDemand(): DemandInterface
+    {
+        return $this->demand;
+    }
+
+    /**
+     * @return QueryInterface
+     */
+    public function getQuery(): QueryInterface
+    {
+        return $this->query;
+    }
+}
