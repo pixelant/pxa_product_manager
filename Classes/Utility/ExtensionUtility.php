043c64a23338ea79ffa190fc39bed8be7dde115a
VladVilya <vladv161095@gmail.com>
2020-12-04 12:33:04 +0200

    [TASK] controller extendability tested and provided

--- Classes/Utility/ExtensionUtility.php
+++ Classes/Utility/ExtensionUtility.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Pixelant\PxaProductManager\Utility;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
 class ExtensionUtility extends \TYPO3\CMS\Extbase\Utility\ExtensionUtility
 {
     /**
@@ -18,12 +16,21 @@ class ExtensionUtility extends \TYPO3\CMS\Extbase\Utility\ExtensionUtility
      */
     public static function addControllerAction(string $extensionName, string $pluginName, string $controllerClassName, array $actions): void
     {
+        $config = &$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers'][$controllerClassName];
+
         foreach ($actions as $action) {
             if (gettype($action) === 'string') {
-                $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers'][$controllerClassName]['actions'][] = $action;
+                $config['actions'][] = $action;
             } else {
                 throw new \UnexpectedValueException('Actions value must be type of string, but type of ' . gettype($action) . ' is given');
             }
         }
+
+        $classNameArray = explode('\\', $controllerClassName);
+        $controller = end($classNameArray);
+        $controllerAlias = current(explode('Controller', $controller));
+
+        $config['alias'] = $controllerAlias;
+        $config['className'] = $controllerClassName;
     }
 }
