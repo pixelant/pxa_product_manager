d90c703dd43737a1acf824e6e2801abab564c408
mabolek <mathias@lilio.com>
2021-01-25 15:58:03 +0100

    [TASK] Relation inheritance also for inline relations

--- ext_tables.sql
+++ ext_tables.sql
@@ -197,3 +197,17 @@ CREATE TABLE tx_pxaproductmanager_product_pages_mm
     KEY uid_local_foreign (uid_local, uid_foreign),
     KEY uid_foreign_tablefield (uid_foreign, tablenames(40), fieldname(3), sorting_foreign)
 );
+
+#
+# Table structure for table 'tx_pxaproductmanager_relation_inheritance_index'
+# Keeps track of inheritance between inline relation records for parent/child products.
+#
+CREATE TABLE tx_pxaproductmanager_relation_inheritance_index
+(
+	uid_parent       int(11)      DEFAULT '0' NOT NULL,
+	uid_child        int(11)      DEFAULT '0' NOT NULL,
+	tablename        varchar(255) DEFAULT ''  NOT NULL,
+
+	KEY uid_child_table (uid_child, tablename),
+	KEY uid_parent_table (uid_parent, tablename)
+);
