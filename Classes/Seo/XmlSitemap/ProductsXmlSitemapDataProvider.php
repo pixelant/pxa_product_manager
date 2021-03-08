f3a57a39733d7e9ed8920e24ebfc9a8a5c93fe8a
Mats Svensson <mats@pixelant.se>
2021-03-08 14:14:03 +0100

    [TASK] fix sitemap
    Also modify it and base it on RecordsXmlSitemapDataProvider instead of using demand from product listings.

--- Classes/Seo/XmlSitemap/ProductsXmlSitemapDataProvider.php
+++ Classes/Seo/XmlSitemap/ProductsXmlSitemapDataProvider.php
@@ -2,52 +2,58 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 namespace Pixelant\PxaProductManager\Seo\XmlSitemap;
 
-use Pixelant\PxaProductManager\Domain\Model\DTO\ProductDemand;
 use Pixelant\PxaProductManager\Domain\Model\Product;
 use Pixelant\PxaProductManager\Domain\Repository\ProductRepository;
 use Pixelant\PxaProductManager\Service\Url\UrlBuilderServiceInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Context\WorkspaceAspect;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryHelper;
+use TYPO3\CMS\Core\Database\Query\Restriction\WorkspaceRestriction;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Seo\XmlSitemap\AbstractXmlSitemapDataProvider;
+use TYPO3\CMS\Seo\XmlSitemap\Exception\MissingConfigurationException;
 
 /**
- * Class ProductsXmlSitemapDataProvider.
+ * ProductXmlSiteDataProvider will provide information for the XML sitemap for products.
  */
-class ProductsXmlSitemapDataProvider extends AbstractXmlSitemapDataProvider
+final class ProductsXmlSitemapDataProvider extends AbstractXmlSitemapDataProvider
 {
-    /**
-     * @var ProductRepository
-     */
-    protected ProductRepository $repository;
-
     /**
      * @var UrlBuilderServiceInterface
      */
     protected UrlBuilderServiceInterface $urlBuilder;
 
     /**
-     * Exclude categories from url.
-     *
-     * @var bool
+     * @var DataMapper
      */
-    protected bool $excludeCategories = false;
-
-    /**
-     * Target url page ID.
-     *
-     * @var int
-     */
-    protected int $pageId = 0;
+    public DataMapper $dataMapper;
 
     /**
      * @param ServerRequestInterface $request
      * @param string $key
      * @param array $config
      * @param ContentObjectRenderer|null $cObj
+     * @throws MissingConfigurationException
      */
     public function __construct(
         ServerRequestInterface $request,
@@ -57,100 +63,146 @@ class ProductsXmlSitemapDataProvider extends AbstractXmlSitemapDataProvider
     ) {
         parent::__construct($request, $key, $config, $cObj);
 
-        $this->excludeCategories = (bool) ($config['url']['excludeCategories'] ?? false);
-        $this->pageId = (int) ($config['url']['pageId'] ?? 0);
-
         $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
-
-        $this->repository = $objectManager->get(ProductRepository::class);
 
         $this->urlBuilder = $objectManager->get(UrlBuilderServiceInterface::class);
         $this->urlBuilder->absolute(true);
+
+        $this->dataMapper = $objectManager->get(DataMapper::class);
 
         $this->generateItems();
     }
 
     /**
-     * Generate site map items.
+     * @throws MissingConfigurationException
      */
-    protected function generateItems(): void
+    public function generateItems(): void
     {
-        $demand = $this->createDemand();
+        $table = ProductRepository::TABLE_NAME;
+        $pids = !empty($this->config['pid']) ? GeneralUtility::intExplode(',', $this->config['pid']) : [];
+        $lastModifiedField = $this->config['lastModifiedField'] ?? 'tstamp';
+        $sortField = $this->config['sortField'] ?? 'sorting';
 
-        $limit = 1000;
-        $offset = 0;
-        do {
-            $demand->setLimit($limit);
-            $demand->setOffSet($offset);
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($table);
 
-            $products = $this->repository->findDemanded($demand);
-
-            /** @var Product $product */
-            foreach ($products as $product) {
-                $this->items[] = [
-                    'product' => $product,
-                    'lastMod' => $product->getTstamp()->getTimestamp(),
-                ];
-            }
-
-            // Increase offset
-            $offset += $limit;
-        } while ($products->count() >= $limit);
-    }
-
-    /**
-     * Storage pids.
-     *
-     * @return array
-     */
-    protected function getStorage(): array
-    {
-        $pids = GeneralUtility::intExplode(',', $this->config['pid'] ?? [], true);
+        $constraints = [];
+        if (!empty($GLOBALS['TCA'][$table]['ctrl']['languageField'])) {
+            $constraints[] = $queryBuilder->expr()->in(
+                $GLOBALS['TCA'][$table]['ctrl']['languageField'],
+                [
+                    -1,
+                    $this->getLanguageId(),
+                ]
+            );
+        }
 
         if (!empty($pids)) {
-            $recursiveLevel = (int)($this->config['recursive'] ?? 0);
+            $recursiveLevel = isset($this->config['recursive']) ? (int)$this->config['recursive'] : 0;
             if ($recursiveLevel) {
-                $newList = [];
-                foreach ($pids as $pid) {
-                    $list = $this->cObj->getTreeList($pid, $recursiveLevel);
-                    if ($list) {
-                        $newList = array_merge($newList, explode(',', $list));
-                    }
-                }
-                $pids = array_merge($pids, $newList);
+                $pids = array_merge($pids, $this->fetchRecursivePids($pids, $recursiveLevel));
+            }
+
+            $constraints[] = $queryBuilder->expr()->in('pid', $pids);
+        }
+
+        if (!empty($this->config['excludedProductTypes'])) {
+            $excludedProductTypes = GeneralUtility::trimExplode(',', $this->config['excludedProductTypes'], true);
+            if (!empty($excludedProductTypes)) {
+                $constraints[] = $queryBuilder->expr()->notIn(
+                    'product_type',
+                    $queryBuilder->createNamedParameter(
+                        $excludedProductTypes,
+                        \TYPO3\CMS\Core\Database\Connection::PARAM_INT_ARRAY
+                    )
+                );
             }
         }
 
-        return $pids;
+        if (!empty($this->config['additionalWhere'])) {
+            $constraints[] = QueryHelper::stripLogicalOperatorPrefix($this->config['additionalWhere']);
+        }
+
+        $queryBuilder->getRestrictions()->add(
+            GeneralUtility::makeInstance(WorkspaceRestriction::class, $this->getCurrentWorkspaceAspect()->getId())
+        );
+
+        $queryBuilder->select('*')
+            ->from($table);
+
+        if (!empty($constraints)) {
+            $queryBuilder->where(
+                ...$constraints
+            );
+        }
+
+        $rows = $queryBuilder->orderBy($sortField)
+            ->execute()
+            ->fetchAll();
+
+        foreach ($rows as $row) {
+            $item = [
+                'data' => $row,
+                'lastMod' => (int)$row[$lastModifiedField],
+                'priority' => 0.5,
+            ];
+            $this->items[] = $item;
+        }
     }
 
     /**
-     * Build product item URL.
-     *
      * @param array $data
      * @return array
      */
     protected function defineUrl(array $data): array
     {
         /** @var Product $product */
-        $product = $data['product'];
+        $product = $this->dataMapper->map(
+            \Pixelant\PxaProductManager\Domain\Model\Product::class,
+            [$data['data']]
+        )[0];
 
-        $data['loc'] = $this->urlBuilder->url(
-            $product
-        );
+        $data['loc'] = $this->urlBuilder->url($product);
 
         return $data;
     }
 
     /**
-     * @return ProductDemand
+     * Fetch recursive pids.
+     *
+     * @param array $pids
+     * @param int $recursiveLevel
+     * @return array
      */
-    protected function createDemand(): ProductDemand
+    protected function fetchRecursivePids(array $pids, int $recursiveLevel): array
     {
-        return GeneralUtility::makeInstance(ProductDemand::class)
-            ->setStoragePid($this->getStorage())
-            ->setOrderBy('tstamp')
-            ->setOrderByAllowed('tstamp')
-            ->setOrderDirection('desc');
+        $newList = [];
+        foreach ($pids as $pid) {
+            $list = $this->cObj->getTreeList($pid, $recursiveLevel);
+            if ($list) {
+                $newList = array_merge($newList, explode(',', $list));
+            }
+        }
+
+        return $newList;
+    }
+
+    /**
+     * @return int
+     * @throws \TYPO3\CMS\Core\Context\Exception\AspectNotFoundException
+     */
+    protected function getLanguageId(): int
+    {
+        $context = GeneralUtility::makeInstance(Context::class);
+
+        return (int)$context->getPropertyFromAspect('language', 'id');
+    }
+
+    /**
+     * @return WorkspaceAspect
+     */
+    protected function getCurrentWorkspaceAspect(): WorkspaceAspect
+    {
+        return GeneralUtility::makeInstance(Context::class)->getAspect('workspace');
     }
 }
