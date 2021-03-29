83401e6d0884ff827a3a0ba819ae453a34dc3ce4
Mathias Bolt Lesniak <mathias@pixelant.no>
2021-03-29 14:36:23 +0200

    [BUGFIX] Set attribute type value to a default fallback (#279)
    The attribute will default to `1` (Input field).

--- Classes/Hook/ProcessDatamap/AttributeTypeValidationProcessDatamap.php
+++ Classes/Hook/ProcessDatamap/AttributeTypeValidationProcessDatamap.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-
 namespace Pixelant\PxaProductManager\Hook\ProcessDatamap;
-
 
 use Pixelant\PxaProductManager\Domain\Model\Attribute;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
@@ -12,8 +10,10 @@ use TYPO3\CMS\Core\Utility\MathUtility;
 
 class AttributeTypeValidationProcessDatamap
 {
+    protected const FALLBACK_TYPE = Attribute::ATTRIBUTE_TYPE_INPUT;
+
     /**
-     * Validate the attribute type value
+     * Validate the attribute type value.
      *
      * @param array $incomingFieldArray
      * @param string $table
@@ -26,21 +26,26 @@ class AttributeTypeValidationProcessDatamap
         string $table,
         string $id,
         DataHandler $dataHandler
-    )
-    {
+    ): void {
         if ($table === 'tx_pxaproductmanager_domain_model_attribute') {
-            if (isset($fieldArray['type']) && !in_array((int)$fieldArray['type'], Attribute::getAttributeTypes())) {
+            if (
+                isset($fieldArray['type'])
+                && !in_array((int)$fieldArray['type'], Attribute::getAttributeTypes(), true)
+            ) {
                 $dataHandler->log(
                     $table,
                     $id,
-                    2, // Update
+                    2,
                     0,
-                    1, // Severity: error
-                    'The attribute type "' . (int)$fieldArray['type'] . '" is not valid.',
+                    1,
+                    'The attribute type "' . (int)$fieldArray['type'] . '" is not valid and was changed to ' .
+                    '"' . self::FALLBACK_TYPE . '".',
                     0,
                     $data = [],
                     (int)$fieldArray['pid']
                 );
+
+                $fieldArray['type'] = self::FALLBACK_TYPE;
 
                 return;
             }
@@ -49,15 +54,17 @@ class AttributeTypeValidationProcessDatamap
                 $dataHandler->log(
                     $table,
                     $id,
-                    1, // New
+                    1,
                     0,
-                    1, // Severity: error
-                    'Attribute type is required.',
+                    1,
+                    'Attribute type is required. It was changed to "' . self::FALLBACK_TYPE . '".',
                     0,
                     $data = [],
                     (int)$fieldArray['pid'],
                     $id
                 );
+
+                $fieldArray['type'] = self::FALLBACK_TYPE;
             }
         }
     }
