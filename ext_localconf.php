15fc94120f25a72d654b6eda02c6b23522978a8c
Mats Svensson <mats.svensson@resultify.se>
2021-06-15 16:20:16 +0200

    [BUGFIX] display crdate and tstamp of products as DateTime in BE lists (#293)
    * [BUGFIX] use correct PageRepository
    * [TASK] clean up unused code
    * [BUGFIX] update TCA to render crdate and tstamp as DateTime instead of timestamp in lists

--- ext_localconf.php
+++ ext_localconf.php
@@ -194,9 +194,4 @@ defined('TYPO3_MODE') || die;
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
         'options.pageTree.doktypesToShowInNewPageDragArea := addToList(' . $pdDokType . ')'
     );
-
-    if (TYPO3_MODE === 'BE') {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers']['pxa_product_manager']
-            = \Pixelant\PxaProductManager\Command\UpdateChildRelationCommandController::class;
-    }
 })();
