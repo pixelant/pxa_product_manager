e89acfe252d471a444313777eabdac01cc490b38
Mats Svensson <mats@pixelant.se>
2021-06-30 09:51:33 +0200

    [!git status][TASK] make attribute-sets localizable

--- Configuration/TCA/tx_pxaproductmanager_domain_model_attributeset.php
+++ Configuration/TCA/tx_pxaproductmanager_domain_model_attributeset.php
@@ -15,6 +15,10 @@ return (function () {
             'dividers2tabs' => true,
             'sortby' => 'sorting',
             'origUid' => 't3_origuid',
+            'transOrigPointerField' => 'l10n_parent',
+            'transOrigDiffSourceField' => 'l10n_diffsource',
+            'languageField' => 'sys_language_uid',
+            'translationSource' => 'l10n_source',
             'delete' => 'deleted',
             'enablecolumns' => [
                 'disabled' => 'hidden',
@@ -23,12 +27,57 @@ return (function () {
             'iconfile' => 'EXT:pxa_product_manager/Resources/Public/Icons/Svg/layers.svg',
         ],
         'types' => [
-            '1' => ['showitem' => 'name, label, layout, attributes, product_types, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, hidden'],
+            '1' => [
+                'showitem' => '
+                    name, label, layout, attributes, product_types,
+                    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language, --palette--;;lang,
+                    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, hidden'
+            ],
         ],
         'palettes' => [
-            '1' => ['showitem' => ''],
+            'lang' => [
+                'showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource'
+            ],
+            '1' => [
+                'showitem' => ''
+            ],
         ],
         'columns' => [
+            'sys_language_uid' => [
+                'exclude' => true,
+                'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+                'config' => [
+                    'type' => 'select',
+                    'renderType' => 'selectSingle',
+                    'special' => 'languages',
+                    'items' => [
+                        [
+                            'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+                            -1,
+                            'flags-multiple',
+                        ],
+                    ],
+                    'default' => 0,
+                ],
+            ],
+            'l10n_parent' => [
+                'displayCond' => 'FIELD:sys_language_uid:>:0',
+                'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+                'config' => [
+                    'type' => 'group',
+                    'internal_type' => 'db',
+                    'allowed' => 'tx_pxaproductmanager_domain_model_attributeset',
+                    'size' => 1,
+                    'maxitems' => 1,
+                    'minitems' => 0,
+                    'default' => 0,
+                ],
+            ],
+            'l10n_diffsource' => [
+                'config' => [
+                    'type' => 'passthrough',
+                ],
+            ],
             'hidden' => [
                 'exclude' => true,
                 'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
