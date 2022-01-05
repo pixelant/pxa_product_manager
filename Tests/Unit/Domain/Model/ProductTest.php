075cd39159ccf8270552f2ae6ae4b2731d84db2f
VladVilya <vladv161095@gmail.com>
2022-01-05 10:38:16 +0200

    [BUGFIX] codestyle fixes

--- Tests/Unit/Domain/Model/ProductTest.php
+++ Tests/Unit/Domain/Model/ProductTest.php
@@ -84,7 +84,8 @@ class ProductTest extends UnitTestCase
         $this->subject->setImages(TestsUtility::createObjectStorage($image1, $image2, $image3));
 
         self::assertSame(
-            $image2, $this->callInaccessibleMethod($this->subject, 'findImagesByType', Image::MAIN_IMAGE)[0]
+            $image2,
+            $this->callInaccessibleMethod($this->subject, 'findImagesByType', Image::MAIN_IMAGE)[0]
         );
     }
 
