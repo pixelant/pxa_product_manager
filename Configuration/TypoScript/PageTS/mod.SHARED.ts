26beb8f1f4606c139c6d683af01bee1640d1a6d0
Mats Svensson <mats@pixelant.se>
2022-01-24 09:52:21 +0100

    [BUGFIX] don't limit column access be default

--- Configuration/TypoScript/PageTS/mod.SHARED.ts
+++ Configuration/TypoScript/PageTS/mod.SHARED.ts
@@ -1,7 +1,8 @@
-[page["doktype"] == 9]
-    mod {
-        SHARED {
-            colPos_list = 50, 7501, 90
-        }
-    }
-[end]
+# Limit access to certain columns.
+#[page["doktype"] == 9]
+#    mod {
+#        SHARED {
+#            colPos_list = 50, 7501, 90
+#        }
+#    }
+#[end]
