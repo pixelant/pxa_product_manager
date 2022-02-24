73bbd1890d2bffb939259e8b0d99687f581524c9
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2022-02-24 14:59:05 +0100

    [WIP] changes to single-view page, sliders, render stacks, js, css

--- Resources/Public/JavaScript/ProductManager.swiper.js
+++ Resources/Public/JavaScript/ProductManager.swiper.js
@@ -1,9 +1,31 @@
 var galleryThumbs = new Swiper('.product-gallery-thumbs', {
   spaceBetween: 10,
-  slidesPerView: 4,
+  slidesPerView: 2,
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  breakpoints: {
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3
+    },
+    // when window width is >= 600px
+    600: {
+      slidesPerView: 4
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 4
+    },
+    // when window width is >= 992px
+    1200: {
+      slidesPerView: 5
+    }
+  }
 });
 
 var galleryTop = new Swiper('.product-gallery-top', {
@@ -20,8 +42,11 @@ var galleryTop = new Swiper('.product-gallery-top', {
 var relatedProducts = new Swiper('.product-relations', {
   slidesPerView: 'auto',
   spaceBetween: 30,
-  pagination: {
-    el: '.product-relations-pagination',
+  scrollbar: {
+    el: '.product-relations-scroll',
+    draggable: false,
+    dragSize: 90,
+    hide: false
   },
   navigation: {
     nextEl: '.product-relations-button-next',
