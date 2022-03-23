c045620572baa890c124ed3f1b60638f5574697c
Mattias Nilsson <tollepjaer@gmail.com>
2022-03-23 09:41:31 +0100

    [TASK] Update demander dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.6-11.5.99',
-            'demander' => '1.0.0-1.99.99'
+            'demander' => '0.1.0-0.2.99'
         ],
         'conflicts' => [],
         'suggests' => []
