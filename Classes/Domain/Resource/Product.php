c31c3da7404eb3d7bb94b66f97d191aed6f5dcae
Mats Svensson <mats@pixelant.se>
2023-01-11 10:29:54 +0100

    [TASK] ci fix

--- Classes/Domain/Resource/Product.php
+++ Classes/Domain/Resource/Product.php
@@ -114,13 +114,16 @@ class Product extends AbstractResource
      */
     protected function addAdditionalAttributes(array &$resource, string $additionalAttributes): void
     {
-        $includeEmptyAdditionalAttributes = (bool)$this->settings['listView']['includeEmptyAdditionalAttributes'] ?? false;
         $additionalAttributesList = GeneralUtility::trimExplode(',', $additionalAttributes, true);
+        $includeEmptyAdditionalAttributes =
+            (bool)$this->settings['listView']['includeEmptyAdditionalAttributes'] ?? false;
+
         foreach ($additionalAttributesList as $attributeIdentifier) {
             $attributeValue = $this->entity->getAttributeValue()[$attributeIdentifier];
             if (!empty($attributeValue)) {
+                $label =  $attributeValue->getAttribute()->getLabel() ?? $attributeValue->getAttribute()->getName();
                 $resource[$attributeIdentifier] = [
-                    'label' => $attributeValue->getAttribute()->getLabel() ?? $attributeValue->getAttribute()->getName(),
+                    'label' => $label,
                     'data' => $attributeValue->getRenderValue(),
                 ];
             } elseif ($includeEmptyAdditionalAttributes) {
